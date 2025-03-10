4526dbe325102b05db8dcb24a46b2c7fa78ea1ed
Djordje Ristic <rist.djordje@gmail.com>
2025-03-10 16:31:39 +0100

    improve high_score code and remove encrypt/decrypt

--- highscore.py
+++ highscore.py
@@ -1,16 +1,21 @@
-import subprocess
+import os
+
 
 def save_score(score):
     with open("high_score.txt", "w") as file:
         file.write(str(score))
-        
-    subprocess.run(["hash_decrypt", "hash"])
+
 
 def get_score():
-    subprocess.run(["hash_decrypt", "decrypt"])
-    
+    if not os.path.exists("high_score.txt"):
+        save_score(0)
+        return 0
+
     with open("high_score.txt", "r") as file:
-        score = file.read()
-    
-    save_score(score)
+        score = file.read().strip()
+
+    if not score.isdigit():
+        save_score(0)
+        return 0
+
     return int(score)
